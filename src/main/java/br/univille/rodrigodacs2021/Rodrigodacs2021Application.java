0ee1793bd0feb0e46271c5c3f927d62d8c259093
piaskowskirodrigo <piaskowskirodrigo@gmail.com>
2021-07-02 18:08:03 +0000

    primeira versao

---
+++ src/main/java/br/univille/rodrigodacs2021/Rodrigodacs2021Application.java
@@ -0,0 +1,13 @@
+package br.univille.rodrigodacs2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rodrigodacs2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rodrigodacs2021Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/rodrigodacs2021/Rodrigodacs2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.rodrigodacs2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rodrigodacs2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
